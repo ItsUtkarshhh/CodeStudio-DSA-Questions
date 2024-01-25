833dde623438723f331d8d3f3cefa2f991a1a161
ItsUtkarshhh <110464977+ItsUtkarshhh@users.noreply.github.com>
2024-01-25 23:45:26 +0530

    Update CodeStudioQ2.cpp

--- Easy 🟢/CodeStudioQ2.cpp
+++ Easy 🟢/CodeStudioQ2.cpp
@@ -1,28 +1,48 @@
-// Question Link : https://www.codingninjas.com/studio/problems/square-root-integral_893351?topList=love-babbar-dsa-sheet-problems&problemListRedirection=true
-int binarySearch (int n) {
-    int s = 0;
-    int e = n;
-    long long int mid = s+ (e-s)/2;
-    long long int ans = -1;
-    while(s<=e) {
-        long long int square = mid*mid;
-        if(square == n) {
+// Question Link : https://www.codingninjas.com/studio/problems/search-in-rotated-sorted-array_1082554?topList=love-babbar-dsa-sheet-problems&problemListRedirection=true
+int binarySearch(vector<int>& arr, int s, int e, int key) {
+    int start = s;
+    int end = e;
+    int mid = (start+end)/2; // More optimized way to use here is, mid = (start + ((end-start)/2)), but okay keep this thing in mind during interviews and coding compettions and online coding! to pass all the test cases!
+    while(start<=end) {
+        if(key==arr[mid]) {
             return mid;
         }
-        if(square < n) {
-            ans = mid;
+
+        if(key>arr[mid]) {
+            start = mid+1;
+        }
+        else {
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+    }
+    return -1;
+}
+
+int pivotElement(vector<int>& arr, int n) {
+    int s = 0; int e = n-1;
+    int mid = s +(e-s)/2;
+    while(s<e) {
+        if(arr[mid]>=arr[0]) {
             s = mid+1;
         }
         else {
-            e = mid-1;
+            e = mid;
         }
-        mid = s+ (e-s)/2;
+        mid = s +(e-s)/2;
     }
-    return ans;
+    return s;
 }
-
-int floorSqrt(int n)
+int search(vector<int>& arr, int n, int k)
 {
-    return binarySearch(n);
     // Write your code here.
+    // Return the position of K in ARR else return -1.
+    int pivot = pivotElement(arr, n);
+    int e = n-1;
+    if(k>=arr[pivot] && k<=arr[e]) {
+        return binarySearch(arr,pivot,n-1,k);
+    }
+    else {
+        return binarySearch(arr, 0, pivot-1, k);
+    }
 }
